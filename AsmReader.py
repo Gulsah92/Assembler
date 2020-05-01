c8bbad1fb42fa3a4698852f1a104d381224a6541
semih <semihsolmaz@hotmail.com>
2020-05-01 19:56:27 +0300

    instruction class dict added

--- AsmReader.py
+++ AsmReader.py
@@ -4,14 +4,16 @@ import string
 
 
 regs = {'A':'0000000000000001', 'B':'0000000000000010', 'C':'0000000000000011', 'D':'0000000000000100', 'E':'0000000000000101', 'S':'0000000000000110', 'PC':'0000000000000000'}
-
-myinst = Instructions.Instruction()
 asm = open('C:\\Users\\semih\\OneDrive\\Desktop\\test.asm', 'r')
 asml = []
+
+myinst = Instructions.Instruction()
 for line in asm:
     line = line.split('\n')
     asml.append(line[0].split(' '))
+
 binl = []
+
 for elem in asml:
     tmpl = [myinst.getInstCode(elem[0]),'','']
     if len(elem) == 2:
