58442fb98349d20a380b76e1acdfe79787980ac4
semih <semihsolmaz@hotmail.com>
2020-05-01 17:01:53 +0300

    instruction class dict added

--- Instructions.py
+++ Instructions.py
@@ -1,10 +1,16 @@
-class Instraction:
+class Instruction:
     def __init__(self):
-        self.inst_list = {'HALT':'0001', 'LOAD': '0002', 'STORE': '0003', 'ADD': '0004', 'SUB': '0005', 'INC': '0006',
-                          'DEC': '0007', 'MUL': '0008', 'DIV': '0009', 'XOR': '000A'}
+        self.inst_list = {'HALT':'00000001', 'LOAD': '00000010', 'STORE': '00000011', 'ADD': '00000100', 'SUB': '00000101', 'INC': '0006',
+                          'DEC': '00000110', 'MUL': '00001000', 'DIV': '00001001', 'XOR': '00001010','AND': '00001011', 'OR':'00001100' ,
+                          'NOT': '00001101', 'SHL': '00001110', 'SHR':'00001111', 'NOP': '00010000','PUSH': '00010001', 'POP': '00010010',
+                          'CMP': '00010011', 'JMP': '00010100', 'JZ':'00010101', 'JE': '00010101','JNZ': '00010110', 'JNE': '00010110',
+                          'JC': '00010111', 'JNC': '00011000', 'JA': '00011001', 'JAE': '00100000','JB': '00100001', 'JBE': '00100010',
+                          'READ': '00100011', 'PRINT': '00100100'}
 
-    def getInstCode(self,instcode):
+    def getInstCode(self, instcode):
         return self.inst_list[instcode]
 
-myinst = Instraction()
+
+myinst = Instruction()
+
 print(myinst.getInstCode('STORE'))
