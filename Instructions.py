28e2a34e526f236e8010154032b3cbf08173530f
GK <60407973+Gulsah92@users.noreply.github.com>
2020-05-01 04:17:07 +0300

    Add files via upload

--- Instructions.py
+++ Instructions.py
@@ -1,6 +1,7 @@
 class Instraction:
     def __init__(self):
-        self.inst_list = {'HALT':'0001', 'LOAD': '0002', 'STORE': '0003'}
+        self.inst_list = {'HALT':'0001', 'LOAD': '0002', 'STORE': '0003', 'ADD': '0004', 'SUB': '0005', 'INC': '0006',
+                          'DEC': '0007', 'MUL': '0008', 'DIV': '0009', 'XOR': '000A'}
 
     def getInstCode(self,instcode):
         return self.inst_list[instcode]
